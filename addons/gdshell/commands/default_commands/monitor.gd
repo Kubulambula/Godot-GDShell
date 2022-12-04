53db0429c7fcaa43774e93fcc5e42efac7de85bc
Jakub Janšta <jansta.ja@gmail.com>
2022-12-04 12:11:34 +0100

    minor improvements

--- addons/gdshell/commands/default_commands/monitor.gd
+++ addons/gdshell/commands/default_commands/monitor.gd
@@ -16,60 +16,59 @@ func _main(argv: Array, _data) -> Dictionary:
 	
 	var monitor_visible_mode: bool = true
 	for i in range(1, argv.size()):
-		if argv[i] == "-m" or argv[i] == "--monitor":
-			monitor_visible_mode = true
-		elif argv[i] == "-u" or argv[i] == "--unmonitor":
-			monitor_visible_mode = false
-		else:
-			match argv[i]:
-				"fps":
-					monitor.set("fps", monitor_visible_mode)
-				"process":
-					monitor.set("process", monitor_visible_mode)
-				"physics_process":
-					monitor.set("physics_process", monitor_visible_mode)
-				"static_memory":
-					monitor.set("static_memory", monitor_visible_mode)
-				"max_static_memory":
-					monitor.set("max_static_memory", monitor_visible_mode)
-				"max_message_buffer":
-					monitor.set("max_message_buffer", monitor_visible_mode)
-				"objects":
-					monitor.set("objects", monitor_visible_mode)
-				"resources":
-					monitor.set("resources", monitor_visible_mode)
-				"nodes":
-					monitor.set("nodes", monitor_visible_mode)
-				"orphan_nodes":
-					monitor.set("orphan_nodes", monitor_visible_mode)
-				"objects_drawn":
-					monitor.set("objects_drawn", monitor_visible_mode)
-				"primitives_drawn":
-					monitor.set("primitives_drawn", monitor_visible_mode)
-				"total_draw_calls":
-					monitor.set("total_draw_calls", monitor_visible_mode)
-				"video_memory":
-					monitor.set("video_memory", monitor_visible_mode)
-				"texture_memory":
-					monitor.set("texture_memory", monitor_visible_mode)
-				"buffer_memory":
-					monitor.set("buffer_memory", monitor_visible_mode)
-				"active_objects_2d":
-					monitor.set("active_objects_2d", monitor_visible_mode)
-				"collision_pairs_2d":
-					monitor.set("collision_pairs_2d", monitor_visible_mode)
-				"islands_2d":
-					monitor.set("islands_2d", monitor_visible_mode)
-				"active_objects_3d":
-					monitor.set("active_objects_3d", monitor_visible_mode)
-				"collision_pairs_3d":
-					monitor.set("collision_pairs_3d", monitor_visible_mode)
-				"islands_3d":
-					monitor.set("islands_3d", monitor_visible_mode)
-				"audio_output_latency":
-					monitor.set("paudio_output_latencys", monitor_visible_mode)
-				_:
-					output("[color=yellow]Unknown monitor '%s'[/color]" % argv[i])
+		match argv[i]:
+			"-m", "--monitor":
+				monitor_visible_mode = true
+			"-u", "--unmonitor":
+				monitor_visible_mode = false
+			"fps":
+				monitor.set("fps", monitor_visible_mode)
+			"process":
+				monitor.set("process", monitor_visible_mode)
+			"physics_process":
+				monitor.set("physics_process", monitor_visible_mode)
+			"static_memory":
+				monitor.set("static_memory", monitor_visible_mode)
+			"max_static_memory":
+				monitor.set("max_static_memory", monitor_visible_mode)
+			"max_message_buffer":
+				monitor.set("max_message_buffer", monitor_visible_mode)
+			"objects":
+				monitor.set("objects", monitor_visible_mode)
+			"resources":
+				monitor.set("resources", monitor_visible_mode)
+			"nodes":
+				monitor.set("nodes", monitor_visible_mode)
+			"orphan_nodes":
+				monitor.set("orphan_nodes", monitor_visible_mode)
+			"objects_drawn":
+				monitor.set("objects_drawn", monitor_visible_mode)
+			"primitives_drawn":
+				monitor.set("primitives_drawn", monitor_visible_mode)
+			"total_draw_calls":
+				monitor.set("total_draw_calls", monitor_visible_mode)
+			"video_memory":
+				monitor.set("video_memory", monitor_visible_mode)
+			"texture_memory":
+				monitor.set("texture_memory", monitor_visible_mode)
+			"buffer_memory":
+				monitor.set("buffer_memory", monitor_visible_mode)
+			"active_objects_2d":
+				monitor.set("active_objects_2d", monitor_visible_mode)
+			"collision_pairs_2d":
+				monitor.set("collision_pairs_2d", monitor_visible_mode)
+			"islands_2d":
+				monitor.set("islands_2d", monitor_visible_mode)
+			"active_objects_3d":
+				monitor.set("active_objects_3d", monitor_visible_mode)
+			"collision_pairs_3d":
+				monitor.set("collision_pairs_3d", monitor_visible_mode)
+			"islands_3d":
+				monitor.set("islands_3d", monitor_visible_mode)
+			"audio_output_latency":
+				monitor.set("paudio_output_latencys", monitor_visible_mode)
+			_:
+				output("[color=yellow]Unknown monitor '%s'[/color]" % argv[i])
 	
 	return DEFAULT_COMMAND_RESULT
 
@@ -83,6 +82,7 @@ func _get_monitor_overlay() -> Node:
 		var monitor: Node = ResourceLoader.load(MONITOR_FILE_PATH, "GDScript").new() as Node
 		monitor.name = StringName(MONITOR_NODE_NAME)
 		monitor.unique_name_in_owner = true
+		monitor.set("fps", false)
 		_PARENT_PROCESS._PARENT_GDSHELL.add_child(monitor)
 	
 	return _PARENT_PROCESS._PARENT_GDSHELL.get_node(NodePath(MONITOR_NODE_NAME))
