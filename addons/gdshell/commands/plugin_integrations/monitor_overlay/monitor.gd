34dc316fb753b95594bd1adc0ee3a90bafd09fff
Jakub Janšta <jansta.ja@gmail.com>
2022-12-05 22:34:35 +0100

    Much cleaner

--- addons/gdshell/commands/plugin_integrations/monitor_overlay/monitor.gd
+++ addons/gdshell/commands/plugin_integrations/monitor_overlay/monitor.gd
@@ -5,70 +5,23 @@ const MONITOR_FILE_PATH: String = "res://addons/monitor_overlay/monitor_overlay.
 const MONITOR_NODE_NAME: String = "GDShellMonitorOverlay"
 
 
-func _init():
-	COMMAND_AUTO_ALIASES = {
-		"unmonitor": "monitor -u",
-	}
-
-
 func _main(argv: Array, _data) -> Dictionary:
 	var monitor: Node = _get_monitor_overlay()
+	var safe_to_edit_properties = _get_safe_to_edit_property_names(monitor)
+	var options: Dictionary = argv_parse_options(argv, true, false)
 	
-	var monitor_visible_mode: bool = true
-	for i in range(1, argv.size()):
-		match argv[i]:
-			"-m", "--monitor":
-				monitor_visible_mode = true
-			"-u", "--unmonitor":
-				monitor_visible_mode = false
-			"fps":
-				monitor.set("fps", monitor_visible_mode)
-			"process":
-				monitor.set("process", monitor_visible_mode)
-			"physics_process":
-				monitor.set("physics_process", monitor_visible_mode)
-			"static_memory":
-				monitor.set("static_memory", monitor_visible_mode)
-			"max_static_memory":
-				monitor.set("max_static_memory", monitor_visible_mode)
-			"max_message_buffer":
-				monitor.set("max_message_buffer", monitor_visible_mode)
-			"objects":
-				monitor.set("objects", monitor_visible_mode)
-			"resources":
-				monitor.set("resources", monitor_visible_mode)
-			"nodes":
-				monitor.set("nodes", monitor_visible_mode)
-			"orphan_nodes":
-				monitor.set("orphan_nodes", monitor_visible_mode)
-			"objects_drawn":
-				monitor.set("objects_drawn", monitor_visible_mode)
-			"primitives_drawn":
-				monitor.set("primitives_drawn", monitor_visible_mode)
-			"total_draw_calls":
-				monitor.set("total_draw_calls", monitor_visible_mode)
-			"video_memory":
-				monitor.set("video_memory", monitor_visible_mode)
-			"texture_memory":
-				monitor.set("texture_memory", monitor_visible_mode)
-			"buffer_memory":
-				monitor.set("buffer_memory", monitor_visible_mode)
-			"active_objects_2d":
-				monitor.set("active_objects_2d", monitor_visible_mode)
-			"collision_pairs_2d":
-				monitor.set("collision_pairs_2d", monitor_visible_mode)
-			"islands_2d":
-				monitor.set("islands_2d", monitor_visible_mode)
-			"active_objects_3d":
-				monitor.set("active_objects_3d", monitor_visible_mode)
-			"collision_pairs_3d":
-				monitor.set("collision_pairs_3d", monitor_visible_mode)
-			"islands_3d":
-				monitor.set("islands_3d", monitor_visible_mode)
-			"audio_output_latency":
-				monitor.set("paudio_output_latencys", monitor_visible_mode)
-			_:
-				output("[color=yellow]Unknown monitor '%s'[/color]" % argv[i])
+	if "options" in options:
+		output("Available monitor options:\n[name : type]")
+		for option in monitor.get_script().get_script_property_list()\
+				.filter(func(x): return x["type"] != TYPE_NIL and x["name"][0] != "_")\
+				.map(func(x): return [x["name"], x["type"]]):
+			output("%s : %s" % option)
+		
+		return DEFAULT_COMMAND_RESULT
+	
+	for option in options:
+		if option in safe_to_edit_properties:
+			monitor.set(option, str_to_var(options[option]))
 	
 	return DEFAULT_COMMAND_RESULT
 
@@ -88,6 +41,12 @@ func _get_monitor_overlay() -> Node:
 	return _PARENT_PROCESS._PARENT_GDSHELL.get_node(NodePath(MONITOR_NODE_NAME))
 
 
+func _get_safe_to_edit_property_names(monitor: Object) -> Array:
+	return monitor.get_script().get_script_property_list()\
+			.filter(func(x): return x["type"] != TYPE_NIL and x["name"][0] != "_")\
+			.map(func(x): return x["name"])
+
+
 func _get_manual() -> String:
 	return """
 [b]NAME[/b]
@@ -97,29 +56,24 @@ func _get_manual() -> String:
 	{COMMAND_AUTO_ALIASES}
 
 [b]SYNOPSIS[/b]
-	monitor [OPTION] [MONITORS]
+	monitor [OPTIONS]
 
 [b]DESCRIPTION[/b]
 	Uses Monitor Overlay plugin by @HungryProton to show various information about performance
 	
-	[b]-m, --monitor[/b]
-		Enables all monitors after this parameter
-		This it the default behaviour if no parameter is specified
-	
-	[b]-u, --unmonitor[/b]
-		Disables all monitors after this parameter
+	[b]-option=value[/b]
+		Sets the option variable of MonitorOverlay node to the value.
+		To see all the available options, run: [i]monitor -options[/i]
 
 [b]EXAMPLES[/b]
-	[i]monitor fps[/i]
+	[i]monitor -fps=true[/i]
 		-Enables fps monitor
-		 Same as monitor -m fps
 	
-	[i]unmonitor fps[/i]
+	[i]unmonitor -fps=false[/i]
 		-Disables fps monitor
-		 Same as monitor -u fps
 	
-	[i]monitor fps process -u physics_process[/i]
-		-Enables fps and process monitors and disabled physics_process monitor
+	[i]monitor -fps=true --process=true --physics_process=false --sampling_rate=10[/i]
+		-Enables fps and process monitors, disables physics_process monitor and sets sampling rate to 10
 """.format({
 	"COMMAND_NAME": COMMAND_NAME,
 	"COMMAND_AUTO_ALIASES": COMMAND_AUTO_ALIASES,
