7326dae18b00236900fe620d976f79b82a61ddfd
Jakub Janšta <jansta.ja@gmail.com>
2023-08-24 23:53:48 +0200

    Improve GDShellCommandDB a bit

--- addons/gdshell/scripts/gdshell_command_db.gd
+++ addons/gdshell/scripts/gdshell_command_db.gd
@@ -7,9 +7,13 @@ var _commands: Dictionary = {}
 var _aliases: Dictionary = {}
 
 
+func has_command(command: String) -> bool:
+	return command in _commands
+
+
 ## Returns the command name on success or empty String on failure
 func add_command(path: String) -> String:
-	var name_and_auto_aliases: Dictionary = GDShellCommandDB.get_command_name_and_auto_aliases(path)
+	var name_and_auto_aliases: Dictionary = {}#GDShellCommandDB.get_command_name_and_auto_aliases(path)
 	if name_and_auto_aliases.is_empty():
 		return ""
 	_commands[name_and_auto_aliases["name"]] = path
@@ -28,6 +32,7 @@ func remove_command(command_name: String) -> void:
 	_commands.erase(command_name)
 
 
+## Returns command path if the command is registered. Returns empty String if not.
 func get_command_path(command_name: String) -> String:
 	return _commands.get(command_name, "")
 
@@ -40,8 +45,20 @@ func get_all_command_names() -> Array[String]:
 	return names
 
 
+func get_all_commands() -> Dictionary:
+	return _commands.duplicate()
+
+
+func has_alias(alias: String) -> bool:
+	return alias in _aliases
+
+
 func add_alias(alias: String, command: String) -> void:
 	_aliases[alias] = command
+
+
+func get_alias_value(alias: String) -> String:
+	return _aliases.get(alias, "")
 
 
 func remove_alias(alias: String) -> void:
@@ -78,34 +95,57 @@ static func _get_file_paths_in_directory(path: String, recursive: bool = true) -
 
 
 static func get_command_file_paths_in_directory(path: String, recursive: bool = true) -> Array[String]:
-	return _get_file_paths_in_directory(path, recursive).filter(
-		func(file_path):
-			return is_file_gdshell_command(file_path)
-	)
+	return _get_file_paths_in_directory(path, recursive).filter(is_file_gdshell_command)
 
 
 static func is_file_gdshell_command(path: String) -> bool:
 	var res: Resource = ResourceLoader.load(path, "GDScript")
 	if not res is GDScript:
 		return false
-	
-	var script: Object = (res as GDScript).new()
-	if not script is GDShellCommand:
-		return false
-	
-	return true
+	return _is_script_gdshell_command(res as GDScript)
 
 
-static func get_command_name_and_auto_aliases(path: String) -> Dictionary:
+static func _is_script_gdshell_command(script: Script) -> bool:
+	# HACK:
+	# This is super error prone. If the name of GDShellCommand script ever changes, this breaks.
+	# I haven't found or figured out a better workaround so this is just to make it work.
+	return script.get_script_property_list().any(
+		func(property: Dictionary) -> bool:
+			return str(property["name"]) == "gdshell_command.gd"
+	)
+
+
+func get_command_name_and_auto_aliases(command: String) -> Dictionary:
+	return (
+		GDShellCommandDB.get_file_command_name_and_auto_aliases(_commands[command]) if command in _commands
+		else {"name": "", "aliases": []}
+	)
+
+
+static func get_file_command_name_and_auto_aliases(path: String) -> Dictionary:
 	var out: Dictionary = {"name": "", "aliases": []}
-	var res: Resource = ResourceLoader.load(path, "GDScript")
-	if not res is GDScript:
+	var command: GDShellCommand = get_file_gdshell_command_instance(path)
+	if command == null:
 		return out
-	
-	var script: Object = (res as GDScript).new()
-	if not script is GDShellCommand:
-		return out
-	
-	out["name"] = (script as GDShellCommand).COMMAND_NAME
-	out["aliases"] = (script as GDShellCommand).COMMAND_AUTO_ALIASES
+	out["name"] = command.COMMAND_NAME
+	out["aliases"] = command.COMMAND_AUTO_ALIASES
 	return out
+
+
+# This is a if monster, but it just checks if the command really exists
+func get_gdshell_command_instance(command_name: String) -> GDShellCommand:
+	var command_script_path: String = get_command_path(command_name)
+	if command_script_path.is_empty():
+		return null
+	return GDShellCommandDB.get_file_gdshell_command_instance(command_script_path)
+
+
+static func get_file_gdshell_command_instance(path: String) -> GDShellCommand:
+	if not is_file_gdshell_command(path):
+		return null
+	
+	var command_script: Resource = ResourceLoader.load(path, "GDScript", ResourceLoader.CACHE_MODE_REUSE)
+	if command_script == null:
+		return null
+	
+	return (command_script as GDScript).new() as GDShellCommand
